a72aa934f6c82d516099a2479a821d6514233569
Ajay957 <ajay74.aa82@gmail.com>
2022-12-09 12:17:36 +0530

    admin login

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+  username=""
+  password=""
+
+  constructor(private route:Router){}
+
+  adminValues=()=>{
+    let data:any={"username":this.username,"password":this.password}
+    if(this.username=="admin" && this.password=="12345"){
+      this.route.navigate(['/view'])
+    }
+    else{
+      alert("the usernme or password is incorrect")
+    }
+  }
+
+}
